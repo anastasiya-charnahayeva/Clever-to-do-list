2565fa6a8a2d59db3ee41580eba027c685207017
anastasiya-charnahayeva <anastasiya-charnahayeva>
2024-05-23 10:12:02 +0300

    env file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,12 +19,12 @@ export default defineNuxtConfig({
   vuefire: {
     auth: true, // enable Firebase Authentication
     config: {
-      apiKey: "AIzaSyCYOSuO8pgs1aP3sqwgzEjCTJU9FPCzAb8",
-      authDomain: "clever-to-do-list-4258a.firebaseapp.com",
-      projectId: "clever-to-do-list-4258a",
-      storageBucket: "clever-to-do-list-4258a.appspot.com",
-      messagingSenderId: "305083695338",
-      appId: "1:305083695338:web:b29e5250dfaf1bfe855793"
+      apiKey: process.env.apikey,
+      authDomain: process.env.authDomain,
+      projectId: process.env.projectId,
+      storageBucket: process.env.storageBucket,
+      messagingSenderId: process.env.messagingSenderId,
+      appId: process.env.appId
     },
   },
   alias: {
